de60a21afc40e5cf195a37bcf455cdca37ce32f9
Ireneusz Cislo <irekc86@o2.pl>
2023-10-16 06:14:27 +0200

    task01 done

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import { Route, Link } from 'react-router-dom';
+
+import Home from './../src/components/Home';
+import Contact from './../src/components/Contact';
+
 const Task01 = () => {
     return (
-        <h1>Task01</h1>
+        <>
+            <h1>Task01</h1>
+            <ul>
+                <li><Link to='/task01/home'>Home</Link></li>
+                <li><Link to='/task01/contact'>Contact</Link></li>
+            </ul>
+            <Route path='/task01/home' component={Home}/>
+            <Route path='/task01/contact' component={Contact}/>
+        </>
     );
 }
 
